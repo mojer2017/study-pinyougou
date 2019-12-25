f7c53477180834c61dd2290060ad5bc5a02cc20a
mojer2017 <827380337@qq.com>
2019-12-25 18:02:37 +0800

    规格管理：新增功能完成

---
+++ PYG/pinyougou-parent/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SpecificationServiceImpl.java
@@ -0,0 +1,113 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbSpecificationMapper;
+import com.pinyougou.mapper.TbSpecificationOptionMapper;
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationExample;
+import com.pinyougou.pojo.TbSpecificationExample.Criteria;
+import com.pinyougou.pojo.TbSpecificationOption;
+import com.pinyougou.pojogroup.Specification;
+import com.pinyougou.sellergoods.service.SpecificationService;
+import entity.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * 服务实现层
+ * @author 毛杰
+ *
+ */
+@Service
+public class SpecificationServiceImpl implements SpecificationService {
+
+	@Autowired
+	private TbSpecificationMapper specificationMapper;
+
+	@Autowired
+	private TbSpecificationOptionMapper specificationOptionMapper;
+	
+	/**
+	 * 查询全部
+	 */
+	@Override
+	public List<TbSpecification> findAll() {
+		return specificationMapper.selectByExample(null);
+	}
+
+	/**
+	 * 按分页查询
+	 */
+	@Override
+	public PageResult findPage(int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);		
+		Page<TbSpecification> page=   (Page<TbSpecification>) specificationMapper.selectByExample(null);
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+
+	/**
+	 * 增加
+	 */
+	@Override
+	public void add(Specification specification) {
+		TbSpecification tbSpecification = specification.getSpecification();
+		specificationMapper.insert(tbSpecification);
+		List<TbSpecificationOption> list = specification.getSpecificationOptionList();
+		for (TbSpecificationOption option : list) {
+			option.setSpecId(tbSpecification.getId());
+			specificationOptionMapper.insert(option);
+		}
+	}
+
+	
+	/**
+	 * 修改
+	 */
+	@Override
+	public void update(TbSpecification specification){
+		specificationMapper.updateByPrimaryKey(specification);
+	}	
+	
+	/**
+	 * 根据ID获取实体
+	 * @param id
+	 * @return
+	 */
+	@Override
+	public TbSpecification findOne(Long id){
+		return specificationMapper.selectByPrimaryKey(id);
+	}
+
+	/**
+	 * 批量删除
+	 */
+	@Override
+	public void delete(Long[] ids) {
+		for(Long id:ids){
+			specificationMapper.deleteByPrimaryKey(id);
+		}		
+	}
+	
+	
+		@Override
+	public PageResult findPage(TbSpecification specification, int pageNum, int pageSize) {
+		PageHelper.startPage(pageNum, pageSize);
+		
+		TbSpecificationExample example=new TbSpecificationExample();
+		Criteria criteria = example.createCriteria();
+		
+		if(specification!=null){			
+						if(specification.getSpecName()!=null && specification.getSpecName().length()>0){
+				criteria.andSpecNameLike("%"+specification.getSpecName()+"%");
+			}
+	
+		}
+		
+		Page<TbSpecification> page= (Page<TbSpecification>)specificationMapper.selectByExample(example);		
+		return new PageResult(page.getTotal(), page.getResult());
+	}
+	
+}
